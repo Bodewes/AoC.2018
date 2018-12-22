6f8a97965d48a6573706200de9627cf21a4c2401
Martijn Bodewes <bodewes@gmail.com>
2018-12-22 11:11:30 +0100

    Solved Day21 and Day22

---
+++ Day21/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solver();
+            s.ParseInput(Input.Data);
+            s.Part1();
+        }
+    }
+
+      public class Solver{
+
+        public Int64[] reg = new Int64[6];
+        int pc = 0;
+
+        int pc_reg = 0;
+
+        List<(OpCode opCode, Int64 a, Int64 b, Int64 c)> prog = new List<(OpCode opCode, Int64 a, Int64 b, Int64 c)>();
+
+        HashSet<long> x = new HashSet<long>();
+        long prev = 0;
+
+        public void Part1(){
+            //Console.WriteLine($"PC Reg = {pc_reg}");
+            var counter = 0;
+            while (pc < prog.Count && pc >= 0){
+                // step
+                // set pc value to reg
+                reg[pc_reg] = pc;
+                //Console.Write($"{pc}\t {PrintReg()} \t {PrintOp(prog[pc])}");
+                ApplyOpCode(prog[pc].opCode, prog[pc].a, prog[pc].b, prog[pc].c);
+                //Console.WriteLine($"\t{PrintReg()} \t{counter++}");
+                pc = (int)reg[pc_reg];
+                pc++;
+                counter++;
+                //Console.WriteLine($"{counter}\t{PrintReg()}");
+                //Console.ReadLine();
+                if(pc == 28){
+                    // First output for part 1
+                    //Console.WriteLine($"\t{PrintReg()}\t {counter} \t {x.Count}");
+                    if (x.Count % 1000 == 0)
+                    {
+                        Console.WriteLine(x.Count);
+                    }
+                    // Part 2
+                    if(!x.Add(reg[3])){
+                        Console.WriteLine($"Last added before looping: {x.Last()} {prev}");
+                        return;
+                    }else{
+                        prev = reg[3];
+                    }
+                }
+                    
+            }
+            PrintReg();
+            Console.WriteLine();
+        }
+
+        public string PrintReg(){
+            return $"[{reg[0]}, {reg[1]}, {reg[2]}, {reg[3]}, {reg[4]}, {reg[5]},]";
+        }
+        public string PrintOp((OpCode opCode, long a, long b, long c) x){
+            return $"{x.opCode} {x.a} {x.b} {x.c}";
+        }
+
+        public void ParseInput(string input){
+            var lines = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+            pc_reg = int.Parse(lines.First().Split(" ")[1]);
+            foreach(var line in lines.Skip(1)){
+                var tokens = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                prog.Add( ( (OpCode)Enum.Parse(typeof(OpCode), tokens[0]), int.Parse(tokens[1]), int.Parse(tokens[2]), int.Parse(tokens[3])));
+            }
+        }
+
+
+
+
+        public void ApplyOpCode(OpCode op, long a, long b, long c){
+            switch(op){
+                case OpCode.addr: Write(c, Read(a)+Read(b)); break;
+                case OpCode.addi: Write(c, Read(a)+b); break;
+
+                case OpCode.mulr: Write(c, Read(a)*Read(b)); break;
+                case OpCode.muli: Write(c, Read(a)*b); break;
+
+                case OpCode.banr: Write(c, Read(a)&Read(b)); break;
+                case OpCode.bani: Write(c, Read(a)&b); break;
+
+                case OpCode.borr: Write(c, Read(a)|Read(b)); break;
+                case OpCode.bori: Write(c, Read(a)|b); break;
+
+                case OpCode.setr: Write(c, Read(a)); break;
+                case OpCode.seti: Write(c, a); break;
+
+                case OpCode.gtir: Write(c, a > Read(b)?1:0); break;
+                case OpCode.gtri: Write(c, Read(a)>b?1:0); break;
+                case OpCode.gtrr: Write(c, Read(a)>Read(b)?1:0); break;
+
+                case OpCode.eqir: Write(c, a == Read(b)?1:0); break;
+                case OpCode.eqri: Write(c, Read(a)==b?1:0); break;
+                case OpCode.eqrr: Write(c, Read(a)==Read(b)?1:0); break;
+
+            }
+        }
+
+        public void Reset(){
+            reg[0] = reg[1] = reg[2] = reg[3] = reg[4] = reg[5] = 0;
+        }
+
+        public long Read(long i){
+            return reg[i];
+        }
+        public void Write(long i, long v){
+            reg[i] = v;
+        }
+
+    }
+
+
+    public enum OpCode{
+        addr = 4,
+        addi = 2,
+        mulr = 12,
+        muli = 3,
+        banr = 15,
+        bani = 5,
+        borr = 1,
+        bori = 0,
+        setr = 7, ///
+        seti = 9,
+        gtir = 14,
+        gtri = 6, ///
+        gtrr = 8,
+        eqir = 10,
+        eqri = 13,
+        eqrr = 11,
+    }
+
+
+    public static class Input{
+        public const string Data = @"#ip 4
+seti 123 0 3
+bani 3 456 3
+eqri 3 72 3
+addr 3 4 4
+seti 0 0 4
+seti 0 2 3
+bori 3 65536 2
+seti 1397714 1 3
+bani 2 255 5
+addr 3 5 3
+bani 3 16777215 3
+muli 3 65899 3
+bani 3 16777215 3
+gtir 256 2 5
+addr 5 4 4
+addi 4 1 4
+seti 27 6 4
+seti 0 6 5
+addi 5 1 1
+muli 1 256 1
+gtrr 1 2 1
+addr 1 4 4
+addi 4 1 4
+seti 25 2 4
+addi 5 1 5
+seti 17 0 4
+setr 5 7 2
+seti 7 4 4
+eqrr 3 0 5
+addr 5 4 4
+seti 5 8 4";
+    }
+}
---
+++ Day22/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solver();
+            // Part1
+            //s.Part1(510, 10, 10); // test
+            //s.Part1(10689, 11, 722);
+            //s.Print();
+
+            // Part2
+            //s.Part1(510, 10, 10, 10); // test
+            s.Part1(10689, 11, 722, 300); // add 300 extra range 
+            s.Part2();
+
+
+        }
+    }
+
+    public class Solver{
+        public long[,] geo;
+        public long[,] ero;
+        public int[,] type;
+
+        int xt, yt;
+
+        public void Part1(long depth, int x_t, int y_t, int extra = 1){
+            xt = x_t;
+            yt = y_t;
+            geo = new long[xt+extra,yt+extra];
+            ero = new long[xt+extra,yt+extra];
+            type = new int[xt+extra,yt+extra];
+
+            for(int y = 0; y< yt+extra; y++){
+                for(int x= 0; x <xt+extra;x++){
+                    if ((x==0 && y==0) || x==xt && y==yt){
+                        geo[x,y] = 0;
+                    }else if (y==0){
+                        geo[x,y] = x*16807;
+                    }else if (x==0){
+                        geo[x,y] = y*48271;
+                    }else{
+                        geo[x,y] = ero[x-1,y]*ero[x,y-1];
+                    }
+                    ero[x,y] = (geo[x,y]+depth)%20183;
+                    type[x,y] = (int) (ero[x,y] %3); // - 0 rocky, 1 wet, 3 narrow
+                }
+            }
+        }
+
+        Dictionary< (int x, int y, int tool),(int g, int h)> open = new Dictionary<(int x, int y, int tool), (int g, int h)>();
+        Dictionary< (int x, int y, int tool),(int g, int h)> closed = new Dictionary<(int x, int y, int tool), (int g, int h)>();
+
+
+        public void Part2(){
+            // tool: 0=neither, 1=torch, 2=climbing gear
+
+            // add start to open
+            open.Add( (0,0,1), (0,dist(0,0)));
+
+            // current (from open list)
+            int x = 0;
+            int y = 0;
+            int t = 1;
+            int d = 0;
+
+            while(open.Count > 0){
+                //Console.WriteLine($"Open: {open.Count}");
+                //Console.WriteLine($"Closed: {closed.Count}");
+
+                var current = open.OrderBy( o=> o.Value.g + o.Value.h ).First();
+                x = current.Key.x;
+                y = current.Key.y;
+                t = current.Key.tool;
+                d = current.Value.g;
+
+                //Console.WriteLine($"Handling: {x},{y} tool:{t} @ distance: {d}");
+
+                if (x == xt && y == yt && t == 1){
+                    Console.WriteLine($"Found path, length: {d}");
+                    return;
+                }
+
+                // remove from open and store in closed
+                open.Remove( current.Key);
+                closed.Add( current.Key, current.Value);
+
+                // try all neighbours with current tool
+                foreach( var n in Neighbours(x,y)){
+
+                    // try with current tool
+                    walkTo(n.x, n.y, d+1, t);
+                }
+                // switch tool
+                if (t !=0)
+                    walkTo(x,y,d+7,0);
+                if (t !=1)  
+                    walkTo(x,y,d+7,1);
+                if (t !=2)
+                    walkTo(x,y,d+7,2);
+               
+                //Console.ReadLine();
+            }
+
+        }
+
+        void walkTo(int nx, int ny, int d, int tool){
+            //Console.Write($"\tTrying {nx},{ny} with {tool} after {d}\t");
+            if (type[nx,ny] == 0 && tool == 0){ /* Console.WriteLine("invalid");*/ return;} // dont allow rocky with neither
+            if (type[nx,ny] == 1 && tool == 1){ /* Console.WriteLine("invalid");*/ return;} // dont allow wet with torch
+            if (type[nx,ny] == 2 && tool == 2){ /* Console.WriteLine("invalid");*/ return;} // dont allow narrow with climbing gear
+
+            // with current tool.
+            if (closed.ContainsKey( (nx,ny,tool))) { /* Console.WriteLine("in closed");*/ return;}  // already here with current tool.
+
+            if (open.ContainsKey ( (nx, ny, tool))){ // kennen we al, zijn we nu sneller?
+                //Console.WriteLine("update open");
+                var (g,h) = open[ (nx, ny, tool)];
+
+                if (d < g ){ // faster! Update,  only compare g (h is the same from here)
+                    open[(nx, ny, tool)] = (d, h);
+                }
+
+            }else{ // nieuw; voeg toe.
+                //Console.WriteLine("new for open");
+                open.Add( (nx, ny, tool), (d, dist(nx, ny)));
+            }
+        }
+
+
+        public IEnumerable<(int x, int y)> Neighbours(int x, int y){
+            if (x>0){
+                yield return (x-1,y);
+            }
+            if (y>0){
+                yield return (x,y-1);
+            }
+            yield return (x+1,y);
+            yield return (x,y+1);
+        }
+
+
+        public int dist(int x, int y){
+            return Math.Abs(xt-x)+Math.Abs(yt-y);
+        }
+
+        public void Print(int extra = 1){
+            int risk = 0;
+            for(int y = 0; y< yt+extra; y++){
+                for(int x= 0; x <xt+extra;x++){
+                    risk += type[x,y];
+                    if (x==0 && y==0)
+                        Console.Write("M");
+                    else if (x==xt && y==yt)
+                        Console.Write("X");
+                    else
+                        Console.Write( type[x,y]==0?'.': type[x,y]==1? '=' : '|' );
+                    //Console.Write(type[x,y]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Total risk: {risk}");
+        }
+
+        public void Print(long[,] data){
+            for(int y = 0; y< xt+1; y++){
+                for(int x= 0; x <xt+1;x++){
+                    // if (x==0 && y==0)
+                    //     Console.Write("M\t");
+                    // else if (x==xt && y==yt)
+                    //     Console.Write("X\t");
+                    // else
+                        //Console.Write( type[x,y]==0?'.': type[x,y]=='1'? '=' : '|' );
+                        Console.Write($"{data[x,y]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
